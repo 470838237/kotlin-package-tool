28e6ada5f3ef0d0d995857d4940d943b1798aa56
heyuan <470838237@qq.com>
2021-01-14 18:34:14 +0800

    args check

--- src/main/java/com/honor/packageTool/Main.java
+++ src/main/java/com/honor/packageTool/Main.java
@@ -6,13 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("exec java main.args="+args.length);
-        for (int i = 0; i <args.length ; i++) {
-            System.out.println(args[i]);
-        }
         com.honor.packageTool.TestKt.main(args);
-
-
-
     }
 
 
--- src/main/java/com/honor/packageTool/Test.kt
+++ src/main/java/com/honor/packageTool/Test.kt
@@ -1,10 +1,53 @@
 package com.honor.packageTool
 
+import com.sun.xml.internal.fastinfoset.util.StringArray
+
+//login -u username -pw password -p platform
+//help
+//action args
+//-pw 密码 -u 用户名 -p 登录平台标识
+
+val actions = arrayOf("login")
 
 fun main(args: Array<String>) {
     println("Hello, World!")
-    args.forEach {
-        print("kotlin:message from java:$it")
+    var ret = checkArgs(args)
+    if (!ret) return
 
+
+}
+
+fun checkArgs(args: Array<String>): Boolean {
+    when (args[0]) {
+        "login" -> {
+            var index = args.indexOf("-u")
+            if (index == -1) {
+                println("")
+            }
+
+        }
+        "help" -> {
+            println(
+                """
+                命令行格式:packager actions [options args]
+                支持的actions包括:help login
+                --------------------------------------------------------------------------------
+                help        输出命令使用帮助
+                login       登录工具账号
+                --------------------------------------------------------------------------------
+                options详情如下:
+                options         参数描述                 所属action参数               是否必须传入
+                -u              登录用户名               login                        是
+                -pw             登录密码                 login                        是
+                -p              登录平台                 login                        是
+            """.trimIndent()
+            )
+        }
+        else -> {
+            println("action输入错误,当前支持action列表:${actions.toString()}")
+        }
     }
+
+
+    return true
 }
